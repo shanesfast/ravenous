ff2ad668431e176e5e03e216027200e7af0223db
Shane Quick <shanesfast@gmail.com>
2018-04-13 15:23:11 -0500

    changed business object property businesses to an array outside of the object to fix props errors

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import BusinessList from "./components/BusinessList/BusinessList.js";
 import SearchBar from "./components/SearchBar/SearchBar.js";
 
 let business = {
-  businesses: [this, this, this, this, this, this],
-
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
@@ -18,13 +16,22 @@ let business = {
   reviewCount: 90
 }
 
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList businesses={ business.businesses } />
+        <BusinessList businesses={ businesses } />
       </div>
     );
   }
